a106ddc6e2a6955262df24971cd958672ab57578
Huibeiq <106383213+Huibq@users.noreply.github.com>
2023-05-18 21:27:00 +0800

    Create qqmusic.py

---
+++ qqmusic/qqmusic.py
@@ -0,0 +1,16 @@
+import requests
+import os
+
+qq = os.environ['qq']
+
+url = f'http://api.qdikun.com/api/qqmusic/?qq={qq}'
+
+response = requests.get(url)
+
+print(response.text)
+
+url =f'https://dachebijia.001api.com/Api/qy?qq={qq}&t=1'
+
+response = requests.get(url)
+
+print(response.json()['msg']+'增加5小时')
