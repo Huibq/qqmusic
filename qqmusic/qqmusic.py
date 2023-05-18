3da22a26f98283c2cf2c594a20463ae7a6f671cf
Huibeiq <106383213+Huibq@users.noreply.github.com>
2023-05-18 22:02:29 +0800

    Update qqmusic.py

--- qqmusic/qqmusic.py
+++ qqmusic/qqmusic.py
@@ -3,8 +3,6 @@ import os
 
 qq = os.environ['qq']
 
-print(qq)
-
 url = f'http://api.qdikun.com/api/qqmusic/?qq={qq}'
 
 response = requests.get(url)
@@ -15,4 +13,4 @@ url =f'https://dachebijia.001api.com/Api/qy?qq={qq}&t=1'
 
 response = requests.get(url)
 
-print(response.json()['msg']+'增加5小时')
+print(response.json()['msg']+'增加8小时')
